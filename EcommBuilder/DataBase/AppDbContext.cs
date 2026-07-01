1cc25406ba889ec93bbd889fce38b48b600dd096
kashef7 <11410120230477@stud.cu.edu.eg>
2026-07-01 19:36:04 +0300

    Added DB folder

---
+++ EcommBuilder/DataBase/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcommBuilder.DataBase;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
--- EcommBuilder/Program.cs
+++ EcommBuilder/Program.cs
@@ -1,8 +1,27 @@
+using EcommBuilder.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+{
+    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString"));
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  => 
+        {
+            policy.WithOrigins("http://localhost:5173");
+        });
+});
 
 var app = builder.Build();
 
@@ -14,28 +33,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors(MyAllowSpecificOrigins);
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
+
+app.MapGet("/api/health",() => "Hello from the backend");
+
+app.MapFallbackToFile("index.html");
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
